61642cd566c6949f9185f6594fb298a2532572b7
AlvaroDazaQuiroga <Alvaro.Daza@fundacion-jala.org>
2016-05-05 12:17:31 -0400

    Tarea2

---
+++ Alvaro/PracticandoRuby.rb
@@ -0,0 +1,7 @@
+puts " este es un puts"
+puts " este es un puts2"
+puts " este es un puts3"
+puts " este es un puts4"
+print " este es un print "
+puts " este es un puts numero 2"
+print " este es un print numero 2 "
